801d0a938a7b21589fb08c737240335bf864ffb9
RafaNandaA <139905333+RafaNandaA@users.noreply.github.com>
2023-09-13 22:33:47 +0700

    Update lampu.c

--- lampu.c
+++ lampu.c
@@ -11,48 +11,55 @@ void setup() {
 
 void loop() {
   //Lampu 6
-  for(int i=0; i<8; i++) {
-    pixels1.setPixelColor(i, pixels1.Color(85, 85, 85));
-  }
-  //Lampu 5 
-  for(int i=8; i<16; i++) {
-    pixels1.setPixelColor(i, pixels1.Color(255, 0, 0));
-  }
-  //Lampu 4-1
-  for(int i=16; i<40; i++) {
-    if(19<i<24){
-      pixels1.setPixelColor(i, pixels1.Color(0, 255, 0));
+  int n=0;
+  while(n<20){
+    if(n>10) {
+      for(int i=16; i<20; i++) {
+        pixels1.setPixelColor(i, pixels1.Color(0, 0, 0));
+      }
+      for(int i=40; i<44; i++) {
+        pixels1.setPixelColor(i, pixels1.Color(0, 0, 0));
+      }
+      pixels1.show(); // Send the updated pixel colors to the hardware
     }
-    else{
-      pixels1.setPixelColor(i, pixels1.Color(85, 85, 85));
+    else {
+      for(int i=16; i<20; i++) {
+        pixels1.setPixelColor(i, pixels1.Color(85, 85, 85));
+      }
+      for(int i=20; i<24; i++) {
+        pixels1.setPixelColor(i, pixels1.Color(0, 255, 0));
+      }
+    //Lampu 4-1
+      for(int i=24; i<39; i++) {
+        pixels1.setPixelColor(i, pixels1.Color(85, 85, 85));
+      }
+      for(int i=40; i<44; i++) {
+        if(i !=39){
+          pixels1.setPixelColor(i, pixels1.Color(85, 85, 85));
+        }
+      } 
+      for(int i=44; i<48; i++) {
+        pixels1.setPixelColor(i, pixels1.Color(255, 0, 0));
+      }
+      pixels1.setPixelColor(39, pixels1.Color(40, 40, 160));//Gara gara lampunya rusak anjing
+      pixels1.show(); 
     }
 
-  }
 
-  for(int i=40; i<44; i++) {
-    if(i !=39){
-      pixels1.setPixelColor(i, pixels1.Color(85, 85, 85));
+    for(int i=0; i<7; i++) {
+      pixels1.setPixelColor(i, pixels1.Color(255, 255, 255));
+      pixels1.setPixelColor(i+1, pixels1.Color(255, 255, 255));
+      pixels1.setPixelColor(i+7, pixels1.Color(255, 0, 0));
+      pixels1.setPixelColor(i+8, pixels1.Color(255, 0, 0));
+      delay(10);
+      pixels1.show();
+      pixels1.setPixelColor(i, pixels1.Color(0, 0, 0));
+      pixels1.setPixelColor(i+1, pixels1.Color(0, 0, 0));
+      pixels1.setPixelColor(i+7, pixels1.Color(0, 0, 0));
+      pixels1.setPixelColor(i+8, pixels1.Color(0, 0, 0));
+      pixels1.show();
     }
-    else{
-      pixels1.setPixelColor(39, pixels1.Color(40, 40, 180));//Gara gara lampunya rusak anjing
-    }
-    
+    n+=1;
   }
 
-  for(int i=44; i<48; i++) {
-    pixels1.setPixelColor(i, pixels1.Color(255, 0, 0));
-  }
-  pixels1.show();
-
-  delay(1000);
-
-  for(int i=20; i<24; i++) {
-    pixels1.setPixelColor(i, pixels1.Color(0, 0, 0));
-  }
-  for(int i=40; i<44; i++) {
-    pixels1.setPixelColor(i, pixels1.Color(0, 0, 0));
-  }
-  pixels1.show(); // Send the updated pixel colors to the hardware
-  // Set color green for the last 4 LEDs (indexes 4-7)
-  delay(1000);
 }
